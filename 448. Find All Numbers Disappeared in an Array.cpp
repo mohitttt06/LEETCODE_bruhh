f2218e4259d1f5a10dbc445e2c1dc1f993f9639d
mohitttt06 <mummyandmom@gmail.com>
2026-05-16 13:56:48 +0530

    Add arrays folder

---
+++ 448. Find All Numbers Disappeared in an Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+       unordered_set <int> s(nums.begin(),nums.end());
+       vector <int> v;
+       
+       for(int i=1;i<=nums.size();i++)
+       {
+
+        if(s.find(i) == s.end())
+        {
+            v.push_back(i);
+        }
+       }
+       return v;
+         
+    }
+};
