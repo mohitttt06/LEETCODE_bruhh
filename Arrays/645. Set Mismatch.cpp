2d4e56ca3a3401380a149b98d87dacb4d6096f48
MOHIT <mummyandmom@gmail.com>
2026-08-15 12:45:05 +0530

    Solved 645. Set Mismatch

---
+++ Arrays/645. Set Mismatch.cpp
@@ -0,0 +1,49 @@
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+
+        map <int,int> mp;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+
+        int first;
+
+        for(auto it:mp)
+        {
+            if(it.second==2)
+            {
+                first = it.first;
+                break;
+
+            }
+        }
+
+        int second;
+
+        set <int> st(nums.begin(),nums.end());
+
+        for(int i=1;i<=nums.size();i++)
+        {
+
+            
+            if(st.find(i)==st.end())
+            {
+                second=i;
+                break;
+                
+
+            }
+        }
+
+        vector <int> v;
+
+        v.push_back(first);
+        v.push_back(second);
+        return v;
+        
+    }
+};
